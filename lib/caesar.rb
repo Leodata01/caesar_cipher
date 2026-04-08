48a0b58d302408973eb078e19a1ebbd931ee114b
leo <187560246+Leodata01@users.noreply.github.com>
2026-04-08 17:44:13 +0700

    - create class Caesar - create method encode

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -1,21 +1,27 @@
 # frozen_string_literal: true
 
-def caesar_cipher(string, key)
-  new_str = ''
-  # iterate through each chr of str.
-  string.each_char do |char|
-    # if letter use algo to return new value
-    new_str += if char.ord.between?(97, 122)
-                 (char.ord - 97 + key % 26 + 97).chr
-               elsif char.ord.between?(65, 90)
-                 ((char.ord - 65 + key) % 26 + 65).chr
-               else
-                 char
-               end
-    # else return chr
+class CaesarCipher
+  def initialize(message, key = 1)
+    @key = key
+    @message = message
+    @encoded_message = ''
   end
-  puts new_str
+
+  def encode
+    # iterate through each chr of str.
+    message.each_char do |char|
+      # if letter use algo to return new value
+      @encoded_message += if char.ord.between?(97, 122)
+                            (char.ord - 97 + key % 26 + 97).chr
+                          elsif char.ord.between?(65, 90)
+                            ((char.ord - 65 + key) % 26 + 65).chr
+                          else
+                            char
+                          end
+      # else return chr
+    end
+  end
 end
 
-caesar_cipher('What a string!', 5)
+# caesar_cipher('What a string!', 5)
 # "Bmfy f xywnsl!"
